8a5dce43ddb29adc0aababa891d15a3618d83774
Charlie Briggs <charliebriggs@gmail.com>
2023-11-14 13:31:04 +0000

    Fix version ranges

--- setup.py
+++ setup.py
@@ -19,7 +19,8 @@
 
 import re
 from os import path
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 
 def read(*paths):
@@ -32,42 +33,42 @@ def find_version(*paths):
     contents = read(*paths)
     match = re.search(r'^__version__ = [\'"]([^\'"]+)[\'"]', contents, re.M)
     if not match:
-        raise RuntimeError('Unable to find version string.')
+        raise RuntimeError("Unable to find version string.")
     return match.group(1)
 
 
 setup(
-      name='tap-redshift',
-      version=find_version('tap_redshift', '__init__.py'),
-      description='Singer.io tap for extracting data from redshift',
-      long_description=read('README.rst'),
-      author='data.world',
-      author_email='help@data.world',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap-redshift'],
-      packages=find_packages(),
-      install_requires=[
-        'attrs==18.2.0',
-        'pendulum>=1.2.0<2.0.0',
-        'singer-python>=5.12.2<6.0.0',
-        'backoff>=1.3.2<2.0.0',
-        'psycopg2>=2.9.3<3.0.0',
-      ],
-      setup_requires=[
-        'pytest-runner>=2.11,<3.0a',
-      ],
-      tests_require=[
-        'pytest-mock>=1.6.3',
-        'mock>=2.0.0',
-        'coverage==4.5.1',
-        'doublex>=1.8.4,<2.0a',
-        'flake8>=2.6.0,<3.4.1a',
-        'pyhamcrest>=1.9.0,<2.0a',
-        'pytest>=3.2.3,<4.0a',
-      ],
-      entry_points={
-        'console_scripts': [
-            'tap-redshift=tap_redshift:main',
+    name="tap-redshift",
+    version=find_version("tap_redshift", "__init__.py"),
+    description="Singer.io tap for extracting data from redshift",
+    long_description=read("README.rst"),
+    author="data.world",
+    author_email="help@data.world",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap-redshift"],
+    packages=find_packages(),
+    install_requires=[
+        "attrs==18.2.0",
+        "pendulum>=1.2.0,<2.0.0",
+        "singer-python>=5.12.2,<6.0.0",
+        "backoff>=1.3.2,<2.0.0",
+        "psycopg2>=2.9.3,<3.0.0",
+    ],
+    setup_requires=[
+        "pytest-runner>=2.11,<3.0a",
+    ],
+    tests_require=[
+        "pytest-mock>=1.6.3",
+        "mock>=2.0.0",
+        "coverage==4.5.1",
+        "doublex>=1.8.4,<2.0a",
+        "flake8>=2.6.0,<3.4.1a",
+        "pyhamcrest>=1.9.0,<2.0a",
+        "pytest>=3.2.3,<4.0a",
+    ],
+    entry_points={
+        "console_scripts": [
+            "tap-redshift=tap_redshift:main",
         ],
-      },
+    },
 )
